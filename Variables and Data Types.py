0e94a732a239f6b41e01623c6b3729e06edeecc5
amang1309 <gurnaniaman1309@gmail.com>
2025-11-17 17:34:39 +0530

    Update 2

--- Variables and Data Types.py
+++ Variables and Data Types.py
@@ -62,6 +62,83 @@ average=sum/2
 print(average)
 print("average=", (num1+num2)/2)
 
+print("Hello, Aman!")
+print ("How are you?"); print ("I am fine here") ; print ('can a line be written in python using single codes?') #each print starts a new line.
+
+print ("I a learning python.",'Its really awesome') #in the same line.
+print ("I am learning python.", end=" ") 
+print ('Its really awesome')  #in the same line using 'end'.
+
+#python variable should only contain alpha numeric number (A-Z, a-z, 0-9) and underscores. 
+
+x,y,z = 5,6,7 
+print (x,y,z) 
+x = y = z = 6
+print (x,y,z)
+print (x+y+z)
+a = "Hello World!" 
+b = "My name is"
+c = "Aman Gurnani" 
+print (a,b,c) 
+#by using '+' to combine variables, we have to manually create spaces. but on using comma ',', we get spaces by default. 
+
+#to make a variable global, we have to declare it global before #initialiazing. 
+
+
+x = "Hello World"	#str	
+x = 20	#int	
+x = 20.5	#float	
+x = 1j	#complex	
+x = ["apple", "banana", "cherry"]	#list	
+x = ("apple", "banana", "cherry")	#tuple	
+x = range(6)	#range	
+x = {"name" : "John", "age" : 36}	#dict	
+x = {"apple", "banana", "cherry"}	#set	
+x = frozenset({"apple", "banana", "cherry"})	#frozenset	
+x = True	#bool	
+x = b"Hello"	#bytes	
+x = bytearray(5)	#bytearray	
+x = memoryview(bytes(5))	#memoryview	
+x = None	#NoneType
+
+x = str("Hello World")	#str	
+x = int(20)	#int	
+x = float(20.5)	#float	
+x = complex(1j)	#complex	
+x = list(("apple", "banana", "cherry"))	#list	
+x = tuple(("apple", "banana", "cherry"))	#tuple	
+x = range(6)	#range	
+x = dict(name="John", age=36)	#dict	
+x = set(("apple", "banana", "cherry"))	#set	
+x = frozenset(("apple", "banana", "cherry"))	#frozenset	
+x = bool(5)	#bool	
+x = bytes(5)	#bytes	
+x = bytearray(5)	#bytearray	
+x = memoryview(bytes(5))	#memoryview
+
+x = 5 + 6j 
+#y = int(x) #complex number can not be casted into another number.
+print (y, type(x))
+print 
+
+x = 5.56 
+y = complex(x) 
+print (y,type(x), type(y))
+
+# MyName = "Aman Gurnani" 
+#y = float (MyName) 
+#print (type(y))
+#so, such type of string can not be converted into int or float.
+MyNum = "45" 
+y = float (MyNum) 
+z= complex (MyNum)
+print (y,z,type(y), type(z))
+
+
+
+
+
+
 
 
 
